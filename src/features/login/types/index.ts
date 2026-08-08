2ca317c8f5bd6dff7d6b235f8727c45d873fe972
fozilovyosin2010 <umnovanika1@gmail.com>
2026-08-08 23:46:15 +0500

    login ui is ready

---
+++ src/features/login/types/index.ts
@@ -0,0 +1,1 @@
+export type { ILoginFormValues } from "./type";
--- src/features/login/ui/Form.tsx
+++ src/features/login/ui/Form.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import clsx from "clsx";
+
 import { Button } from "@/src/shared/model/shadcn/ui/button";
 import { Input } from "@/src/shared/model/shadcn/ui/input";
 
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ILoginFormValues } from "../types/index";
 
 const inpList = [
   {
@@ -21,37 +24,51 @@ const LoginForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+    watch,
+  } = useForm<ILoginFormValues>();
 
-  function hanLogin(data) {
+  const hanLogin: SubmitHandler<ILoginFormValues> = (data) => {
     console.log(data);
-  }
+  };
+
+  // check that inputs are not free
+  const userNameInp = watch("username")?.trim().length;
+  const passwordInp = watch("password")?.trim().length;
 
   return (
     <form
       onSubmit={handleSubmit(hanLogin)}
       className="flex flex-col justify-between gap-[50px]"
     >
-      {/* here  fix it*/}
-
-      {/* <input type="text" {...register("username")} /> */}
       <div className="flex flex-col gap-3">
         {inpList.map((e) => {
           return (
             <div key={e.name} className="relative rounded-md border group">
-              <span className="group-focus-within:-top-3 px-2 pointer-events-none duration-300 bg-[#fff] absolute left-0 top-2 h-[20px] ml-2 text-[#857a7a] text-[16px] truncate z-20">
+              <span
+                className={clsx(
+                  "group-focus-within:top-0 group-focus-within:text-[14px] pointer-events-none duration-300 px-2",
+                  (e.name == "username" && !userNameInp) ||
+                    (e.name == "password" && !passwordInp)
+                    ? "bg-[#fff] absolute left-0 top-3 ml-2 text-[#857a7a] text-[16px] truncate z-20"
+                    : // userNameInp &&
+                      "bg-[#fff] absolute left-0 top-0 ml-2 px-2 text-[#857a7a] text-[14px] truncate z-20",
+                )}
+              >
                 {e.placeholder}
               </span>
               <Input
-                {...register(e.name)}
-                className="p-[20px] w-full border-none group-focus-within:pt-[25px] bg-[#fff] relative z-10"
+                {...register(e.name as "username" | "password")}
+                className="p-[20px] w-full border-none bg-[#fff] text-[18px] relative z-10 outline-none pt-[30px]"
                 type={e.name === "password" ? "password" : "text"}
               />
             </div>
           );
         })}
       </div>
-      <Button className="rounded-[10px] p-[16px_32px] font-[500] text-[16px]">
+      <Button
+        type="submit"
+        className="rounded-[10px] p-[16px_32px] font-[500] text-[16px]"
+      >
         Log in
       </Button>
     </form>
